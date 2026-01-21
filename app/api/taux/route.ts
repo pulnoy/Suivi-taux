a53a480da5355bd55f1a00007b6b5b7bc547e375
pulnoy <pulnoy@gmail.com>
2026-01-21 21:18:33 +0100

    Modify GET response data in route.ts
    Updated the oat10 value and added inflation data to the response.

--- app/api/taux/route.ts
+++ app/api/taux/route.ts
@@ -1,27 +1,26 @@
-
 // app/api/taux/route.ts
+// Mis à jour automatiquement par GitHub Actions
+// Dernière mise à jour: 21/01/2026
 
 export async function GET() {
-  // TODO: remplace ces valeurs par tes vraies données si besoin
   const data = {
-    asof: "21/01/2026",
-    estr: 1.93,
-    oat10: 3.54,
-    cac5: 11.41,
-    scpi5: 4.48
-  };
+    "asof": "21/01/2026",
+    "estr": 1.93,
+    "oat10": 3.56,
+    "cac5": 8.4,
+    "scpi5": 4.48,
+    "inflation": 2
+};
 
   return new Response(JSON.stringify(data), {
     status: 200,
     headers: {
       "Content-Type": "application/json; charset=utf-8",
-      // Autorise l'accès depuis un fichier HTML ouvert en local (Origin: null)
       "Access-Control-Allow-Origin": "*"
     }
   });
 }
 
-// (facultatif, mais propre) : répond au préflight CORS si le navigateur en envoie un
 export async function OPTIONS() {
   return new Response(null, {
     status: 204,
